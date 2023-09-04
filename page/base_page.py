3a8558f46e230c91cad85c392095a70d53bde252
DmitriYESS <dpetrakov97@gmail.com>
2023-09-04 04:07:33 +0300

    Небольшие поправки

--- page/base_page.py
+++ page/base_page.py
@@ -16,36 +16,42 @@ class WorkPage:
 
     def open_element_page(self):
 
-        button = self.browser.find_element(*BasePageLocators.BUTTON_ELEMENTS)
         self.browser.implicitly_wait(5)
+        button = self.browser.find_element(*BasePageLocators.BUTTON_ELEMENTS)
         assert button, "button is not presented"
         button.click()
 
     def open_checkbox(self):
 
-        checkbutton = self.browser.find_element(*BasePageLocators.CHECKBOX)
         self.browser.implicitly_wait(5)
+        checkbutton = self.browser.find_element(*BasePageLocators.CHECKBOX)
         assert checkbutton, "button is not presented"
         checkbutton.click()
 
     def open_home(self):
 
-        home_button = self.browser.find_element(*BasePageLocators.HOME)
         self.browser.implicitly_wait(5)
+        home_button = self.browser.find_element(*BasePageLocators.HOME)
         assert home_button, "button is not presented"
         home_button.click()
 
     def open_downloads(self):
 
-        download_button = self.browser.find_element(*BasePageLocators.DOWNLOADS)
         self.browser.implicitly_wait(5)
+        download_button = self.browser.find_element(*BasePageLocators.DOWNLOADS)
         assert download_button, "button is not presented"
+        self.browser.execute_script("return arguments[0].scrollIntoView(true);", download_button)
+        time.sleep(2)
         download_button.click()
 
     def chose_word_file(self):
 
-        word_file_button = self.browser.find_element(*BasePageLocators.CHECKBOX_WORD_FILE)
-        self.browser.implicitly_wait(5)
-        assert word_file_button, "button is not presented"
-        word_file_button.click()
-
+        try:
+            word_file_button = self.browser.find_element(*BasePageLocators.CHECKBOX_WORD_FILE)
+        except NoSuchElementException:
+            self.browser.find_element(*BasePageLocators.DOWNLOADS).click()
+            self.browser.implicitly_wait(5)
+            word_file_button = self.browser.find_element(*BasePageLocators.CHECKBOX_WORD_FILE)
+            assert word_file_button, "button is not presented"
+            word_file_button.click()
+            print('You have selected:wordFile')
